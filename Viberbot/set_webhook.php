12131b2c7f4568aecd59539d4a18c7158c50233a
uawestllc <81701563+uawestllc@users.noreply.github.com>
2021-03-31 19:00:05 +0300

    Add files via upload

---
+++ Viberbot/set_webhook.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4d1d268f6e67d2ff-667bd394346d64dd-3088501e36b662a7';
+	$webhook = 'uawestllc.github.io/index.php';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
